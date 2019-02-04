1d18d10561fd285e9f7ad5a0c438aea6b4bafb80
ori7 <orisuliman@gmail.com>
2019-02-04 16:28:39 +0200

    Arrange the code

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -37,35 +37,7 @@ export class EventsService {
   ]
 
   get(): EventModel[] {
-    return [
-      {
-        id: 1,
-        name: 'Tu-bishvat',
-        date: new Date(2019, 0, 20),
-        duration: 2,
-        classes: [
-          'a', 'b', 'c'
-        ]
-      },
-      {
-        id: 2,
-        name: 'Purim',
-        date: new Date(2019, 1, 15),
-        duration: 5,
-        classes: [
-          'd', 'e'
-        ]
-      },
-      {
-        id: 3,
-        name: 'Purim 2',
-        date: new Date(2019, 1, 15),
-        duration: 4,
-        classes: [
-          'a', 'b'
-        ]
-      }
-    ];
+    return this.events;
   }
 
   saveInLocalStorage(): void {
